9344eedc68a73ca019c684547016b632fd80f064
AC-Dc <changdavidson4@gmail.com>
2021-08-27 13:26:18 -0400

    don't error on spaces

--- Commands/mass-payout.js
+++ Commands/mass-payout.js
@@ -16,7 +16,7 @@ module.exports = class extends Command {
         const balances = args.split('\n').map(s => {const t = s.split(' '); const n = t.pop(); return [t.join(' '), n]});
 
         const lengthError = balances.findIndex(b => b.length < 2) + 1;
-        const numberError = balances.findIndex(([u, n]) => !n.match(/-?\d+/)) + 1;
+        const numberError = balances.findIndex(([u, n]) => !n.match(/^-?\d+$/)) + 1;
 
         if (lengthError) return await channel.send(
             `Line ${lengthError} only has 1 part. I need the user and the amount separated by a space.`
@@ -26,13 +26,17 @@ module.exports = class extends Command {
         );
 
         const errors = (await Promise.all(balances.map(async ([u, n], i) => {
-            return await client.unb.editUserBalance(
-                guild.id,
-                (await guild.members.fetch(u.match(/\d{17,21}/)?.[0]).then(m => m.id).catch(err => null)) ||
-                    (await guild.members.search({ query: u.split('#')[0] }))
-                    .find(m => u.split('#')[1] ? m.user.discriminator === u.split('#')[1] : true).id,
-                { cash: Number(n) }
-            ).then(() => null).catch(() => i + 1);
+            try {
+                return await client.unb.editUserBalance(
+                    guild.id,
+                    (await guild.members.fetch(u.match(/\d{17,21}/)?.[0]).then(m => m.id).catch(err => null)) ||
+                        (await guild.members.search({ query: u.split('#')[0] })
+                        .then(mem => mem.find(m => u.split('#')[1].trim() ? m.user.discriminator === u.split('#')[1].trim() : true)?.id)),
+                    { cash: Number(n) }
+                ).then(() => null).catch(() => i + 1);
+            } catch(err) {
+                return i + 1;
+            }
         }))).filter(e => e);
 
         return await channel.send(
