d5bb2e5716006cb4dc58dbb8590117b43d13615b
AC-Dc <changdavidson4@gmail.com>
2021-08-27 12:31:16 -0400

    update lb command name and format

--- Commands/exportlb.js
+++ Commands/exportlb.js
@@ -4,14 +4,14 @@ module.exports = class extends Command {
     constructor(client, name) {
         super(client, name, {
             description: 'Get all balances.',
-            aliases: ['lb'],
+            aliases: ['export'],
             guildOnly: true,
         });
     }
 
     async run({ client, message, channel, member, guild, args }) {
         const lb = await client.unb.getGuildLeaderboard(guild.id);
-        const csv = lb.map(u => `${u.user_id}, ${u.cash}, ${u.bank}`).join('\n');
+        const csv = lb.map(u => `${u.user_id}, ${u.cash}, ${u.bank},`).join('\n');
         return await channel.send({ files: [{ name: 'leaderboard.txt', attachment: Buffer.from(csv) }] });
     }
 }
